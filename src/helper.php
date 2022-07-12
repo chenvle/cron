9a9852f9d8b5b243a66844961a712e9f24fa1529
chenvle <chenvle@cmtime.cn>
2022-07-12 14:30:17 +0800

    优化

--- src/helper.php
+++ src/helper.php
@@ -4,7 +4,7 @@ use chenvle\cron\command\Run;
 use chenvle\cron\command\Schedule;
 
 //自定义命令行失败则注释
-// \think\Console::addDefaultCommands([
-//     Run::class,
-//     Schedule::class,
-// ]);
+\think\Console::addDefaultCommands([
+    Run::class,
+    Schedule::class,
+]);
